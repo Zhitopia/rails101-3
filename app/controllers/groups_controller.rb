6317b0816293d9b62452baa73285c0d5deb1f20a
Zhang mickey <Zhitopia@Gmail.com>
2017-07-12 22:51:13 +0800

    implement my posts

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! ，only: [:new, :create, :edit, :update, :destroy, :join, :quit]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy, :join, :quit]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
